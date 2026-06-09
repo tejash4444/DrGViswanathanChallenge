be53df1268b5236a161160651cee9ef9ac744eeb
Tejash Poddar <tejahpoddarvis@gmail.com>
2026-06-09 19:19:26 +0530

    TwoSum

---
+++ TwoSum.java
@@ -0,0 +1,24 @@
+class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        TwoSum s = new TwoSum();
+
+        int[] ans = s.twoSum(
+            new int[]{2, 7, 11, 15},
+            9
+        );
+
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+}
