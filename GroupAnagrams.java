894477b44d0c051cc02ddd198e41d64379a34d7f
Tejash Poddar <tejahpoddarvis@gmail.com>
2026-06-11 23:28:13 +0530

    GroupAnagrams

---
+++ GroupAnagrams.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Solution {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
+
+            char[] arr = strs[i].toCharArray();
+
+            Arrays.sort(arr);
+
+            String key = new String(arr);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(strs[i]);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+/* MAIN FUNCTION IS WRITTEN BY AI */
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        List<List<String>> result = sol.groupAnagrams(strs);
+
+        System.out.println(result);
+    }
+}
